6a9f92fba29626333d894b889b79d37938a8e376
Volstagg-the-Valiant <93149774+Volstagg-the-Valiant@users.noreply.github.com>
2023-11-05 10:30:08 -0600

    gts9 device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gts9wifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gts9wifi-user \
+    omni_gts9wifi-userdebug \
+    omni_gts9wifi-eng
---
+++ omni_gts9wifi.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from gts9wifi device
+$(call inherit-product, device/samsung/gts9wifi/device.mk)
+
+PRODUCT_DEVICE := gts9wifi
+PRODUCT_NAME := omni_gts9wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-X710
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gts9wifixx-user 13 TP1A.220624.014 X710XXU1AWHA release-keys"
+
+BUILD_FINGERPRINT := samsung/gts9wifixx/gts9wifi:13/TP1A.220624.014/X710XXU1AWHA:user/release-keys
